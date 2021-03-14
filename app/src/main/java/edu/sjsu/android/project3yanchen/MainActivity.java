3d53a8807fdd9f75aee1b91d0318db10a5e88027
Yan Chen <yan.chen01@sjsu.edu>
2021-03-14 05:45:06 +0000

    Fix Typo

--- app/src/main/java/edu/sjsu/android/project3yanchen/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/project3yanchen/MainActivity.java
@@ -3,7 +3,7 @@ package edu.sjsu.android.project3yanchen;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     // TODO: add a MyView attribute
 
